0c60396f6266e609fe17c8ea73889ef780967d5b
Kunal Yadav <kunal780954@gmail.com>
2026-04-08 22:01:02 +0530

    added launch script & gitignore

---
+++ src/edge_robot_description/launch_script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Script to run the description package with one file
+
+cleanup() {
+    echo "Cleaning up..."
+    pkill -f "ros2|rviz2|robot_state_publisher|joint_state_publisher"
+}
+
+trap 'cleanup' SIGINT SIGTERM
+
+echo "Sourcing ROS2..."
+source /opt/ros/jazzy/setup.bash
+source ~/edge_ws/install/setup.bash
+
+echo "Launching URDF in RViz..."
+ros2 launch urdf_tutorial display.launch.py \
+    model:=/home/kunal-jazzy/edge_ws/src/edge_robot_description/urdf/rover.urdf.xacro
+
+wait
