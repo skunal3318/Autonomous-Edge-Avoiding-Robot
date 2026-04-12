72dc922b737b22c3a1d0d2c45f0643a58250a7d8
Kunal Yadav <kunal780954@gmail.com>
2026-04-12 17:42:26 +0530

    updated launch_script to launch simulation

--- build/edge_ws/colcon_command_prefix_build.sh
+++ build/edge_ws/colcon_command_prefix_build.sh
@@ -1,2 +1,3 @@
 # generated from colcon_core/shell/template/command_prefix.sh.em
 . "/home/kunal-jazzy/edge_ws/install/edge_robot_description/share/edge_robot_description/package.sh"
+. "/home/kunal-jazzy/edge_ws/install/edge_robot_bringup/share/edge_robot_bringup/package.sh"
--- src/edge_robot_description/launch_script.sh
+++ src/edge_robot_description/launch_script.sh
@@ -12,8 +12,12 @@ echo "Sourcing ROS2..."
 source /opt/ros/jazzy/setup.bash
 source ~/edge_ws/install/setup.bash
 
-echo "Launching URDF in RViz..."
-ros2 launch urdf_tutorial display.launch.py \
-    model:=/home/kunal-jazzy/edge_ws/src/edge_robot_description/urdf/rover.urdf.xacro
+# echo "Launching URDF in RViz..."
+# ros2 launch urdf_tutorial display.launch.py \
+#     model:=/home/kunal-jazzy/edge_ws/src/edge_robot_description/urdf/rover.urdf.xacro
+
+
+echo "Launching gazebo and Rviz..."
+ros2 launch edge_robot_bringup robot.launch.xml 
 
 wait
